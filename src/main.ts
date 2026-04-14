324ad152a3215b839c53059be47fd98a83fbc897
Maxime Reynier <maxime.rnr@gmail.com>
2026-04-14 20:01:11 +0200

    feat: extract Vue Router to dedicated module

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createRouter, createWebHistory } from 'vue-router'
+import router from './router'
 import App from './App.vue'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: () => import('./views/CharacterList.vue') },
-    { path: '/character/new', component: () => import('./views/CharacterCreate.vue') },
-  ],
-})
 
 const app = createApp(App)
 app.use(createPinia())
